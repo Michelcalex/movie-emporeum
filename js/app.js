ab4794af2855fd43f7492dc3746abf4b3b2cb834
Michelcalex <Michelcalex+github@gmail.com>
2017-01-26 23:58:59 -0500

    Started rating functionality

--- js/app.js
+++ js/app.js
@@ -3,12 +3,17 @@ const app = angular.module('MovieApp', []);
 
 app.controller('GetMoviesController', function($scope, MovieService) {
     $scope.movies = MovieService.getAll();
+
+    $scope.rating = function(target) {
+        console.log('clicking');
+        MovieService.giveRating(target);
+    };
 });
 
 app.factory('MovieService', function($http) {
     const movies = [];
 
-    $http.get('https://api.themoviedb.org/3/discover/movie?api_key=1c3ab4a786313cd93564b50c97e21734&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1').then(function (response) {
+    $http.get('https://api.themoviedb.org/3/discover/movie?api_key=1c3ab4a786313cd93564b50c97e21734&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false').then(function (response) {
         angular.copy(response.data.results, movies);
     });
 
@@ -19,5 +24,9 @@ app.factory('MovieService', function($http) {
         getAll() {
             return movies;
         },
+
+        giveRating(thisMovie) {
+            thisMovie.isRated = true;
+        }
     }
 });
--- public/app.js
+++ public/app.js
@@ -4,12 +4,17 @@ const app = angular.module('MovieApp', []);
 
 app.controller('GetMoviesController', function($scope, MovieService) {
     $scope.movies = MovieService.getAll();
+
+    $scope.rating = function(target) {
+        console.log('clicking');
+        MovieService.giveRating(target);
+    };
 });
 
 app.factory('MovieService', function($http) {
     const movies = [];
 
-    $http.get('https://api.themoviedb.org/3/discover/movie?api_key=1c3ab4a786313cd93564b50c97e21734&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1').then(function (response) {
+    $http.get('https://api.themoviedb.org/3/discover/movie?api_key=1c3ab4a786313cd93564b50c97e21734&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false').then(function (response) {
         angular.copy(response.data.results, movies);
     });
 
@@ -20,6 +25,10 @@ app.factory('MovieService', function($http) {
         getAll() {
             return movies;
         },
+
+        giveRating(thisMovie) {
+            thisMovie.isRated = true;
+        }
     }
 });
 },{}]},{},[1]);
